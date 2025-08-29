5c792d4d5af52686f432ffbc7ba33981ecbdef10
Nithingowtham129 <132273083+Nithingowtham129@users.noreply.github.com>
2025-08-29 18:11:30 +0530

    Update Account.java

--- src/main/java/com/example/demo/entity/Account.java
+++ src/main/java/com/example/demo/entity/Account.java
@@ -2,6 +2,7 @@ package com.example.demo.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
